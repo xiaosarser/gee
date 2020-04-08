d4e5870dfe3b1d40ad99887c8c925caf2c35e267
xiaosarser <774058139@qq.com>
2020-04-07 19:33:13 -0700

    Add files via upload

--- gee/gee.go
+++ gee/gee.go
@@ -1,21 +1,17 @@
 package gee
 
-import(
-	"fmt"
+import (
 	"net/http"
 )
-type HandlerFunc func(http.ResponseWriter,*http.Request)
+type HandlerFunc func(*Context)
 type Engine struct {
-	router map[string]HandlerFunc
+	router *router
 }
-
 func New()* Engine{
-	return &Engine{router:make(map[string]HandlerFunc)}
+	return &Engine{router:newRouter()}
 }
 func(engine *Engine)addRoute(method string,pattern string,handler HandlerFunc) {
-	key:=method+"-"+pattern
-	engine.router[key]=handler
-
+	engine.router.addRoute(method, pattern, handler)
 }
 func(engine *Engine)Get(pattern string, handler HandlerFunc){
 	engine.addRoute("GET",pattern, handler)
@@ -27,12 +23,8 @@ func (engine *Engine)RUN(addr string)(err error){
 	return http.ListenAndServe(addr,engine)
 }
 func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	key := req.Method + "-" + req.URL.Path
-	if handler, ok := engine.router[key]; ok {
-		handler(w, req)
-	} else {
-		fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
-	}
+	c :=newContext(w,req)
+	engine.router.handle(c)
 }
 
 
